4c652e5b5e604d80c1b10e2a7498c5a79716eec3
iToussaint <iradukundatoussaint01@gmail.com>
2026-03-25 02:20:58 +0200

    Offline posts manager app finalised

--- lib/database_helper.dart
+++ lib/database_helper.dart
@@ -1,8 +1,6 @@
+import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart';
 import 'post_model.dart';
-import 'dart:io';
-import 'package:flutter/foundation.dart' show kIsWeb;
-import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 
 class DatabaseHelper {
   static final DatabaseHelper instance = DatabaseHelper._init();
@@ -17,17 +15,11 @@ class DatabaseHelper {
   }
 
   Future<Database> _initDB(String filePath) async {
-    if (!kIsWeb) {
-      if (Platform.isWindows || Platform.isLinux) {
-        sqfliteFfiInit();
-        databaseFactory = databaseFactoryFfi;
-      }
-    } else {
-      throw Exception("SQLite is not supported on the Web for this lab.");
-    }
-
+    // Get the standard mobile database directory
     final dbPath = await getDatabasesPath();
     final path = join(dbPath, filePath);
+
+    // Open the database with standard mobile factory
     return await openDatabase(path, version: 1, onCreate: _createDB);
   }
 
@@ -41,17 +33,20 @@ class DatabaseHelper {
     ''');
   }
 
+  // CRUD: Create
   Future<int> create(Post post) async {
     final db = await instance.database;
     return await db.insert('posts', post.toMap());
   }
 
+  // CRUD: Read All
   Future<List<Post>> readAllPosts() async {
     final db = await instance.database;
     final result = await db.query('posts', orderBy: 'id DESC');
     return result.map((json) => Post.fromMap(json)).toList();
   }
 
+  // CRUD: Update
   Future<int> update(Post post) async {
     final db = await instance.database;
     return db.update(
@@ -62,6 +57,7 @@ class DatabaseHelper {
     );
   }
 
+  // CRUD: Delete
   Future<int> delete(int id) async {
     final db = await instance.database;
     return await db.delete('posts', where: 'id = ?', whereArgs: [id]);
